ca5b5e8c557b54a428371dafa380fec993265427
Dalin Aranga <dalinaranga@gmail.com>
2022-06-12 22:03:39 +0530

    Final

---
+++ react frontend/src/component/FooterComponent.js
@@ -0,0 +1,26 @@
+
+import React, { Component } from 'react';
+
+class FooterComponent extends Component {
+    constructor(props){
+        super(props)
+
+        this.state={
+    
+        }
+    }
+    render() {
+        return (
+            <div>
+                <br></br>
+                 
+                <footer className="footer">
+                    <span className="text-muted">Create By MDA PERERA 3231</span>
+                </footer>
+    
+            </div>
+        );
+    }
+}
+
+export default FooterComponent;
